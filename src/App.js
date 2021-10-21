008f62f47f2fa8ada4583923b9e43533c99f869a
Guilherme Watanabe <watanab430@gmail.com>
2021-10-21 13:20:23 -0400

    Feat: create the voting page.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import { BaseContainer } from "./components/UI";
 import AnimalList from "./pages/AnimalList/AnimalList";
+import Voting from "./pages/Voting/Voting";
 
 const App = () => {
     return (
@@ -17,6 +18,9 @@ const App = () => {
                 </Route>
                 <Route path="/register">
                     <Register />
+                </Route>
+                <Route path="/voting">
+                    <Voting />
                 </Route>
                 <Route path="/list">
                     <AnimalList />
--- src/components/UI/variables.js
+++ src/components/UI/variables.js
@@ -1,4 +1,5 @@
 
+export const lightRed = "#ef3038";
 export const lightGreen = "#B2F77D";
 export const darkGreen = "#75BD4B";
 export const baseRadius = "8px";
